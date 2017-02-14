66012bf91cd9f2132ceecd2da4caafe7b542d1fd
vpish <vpish02@hotmail.com>
2017-02-14 17:21:13 +0200

    fixed home button

--- src/test/java/com/lohika/gap/pages/HomePage.java
+++ src/test/java/com/lohika/gap/pages/HomePage.java
@@ -12,7 +12,7 @@ public class HomePage extends BasePage {
 
     @FindBy(xpath = "//a[@aria-label='Create new…']") private WebElement plusDropdown;
     @FindBy(xpath = "//a[@class= 'dropdown-item' and @href='/new']") private WebElement newRepositoryItem;
-    @FindBy(css = "svg[height='30']") private WebElement homeButton;
+    @FindBy(css = "a.header-logo-invertocat") private WebElement homeButton;
     @FindBy(css = "div.dashboard-sidebar.column.one-third") private WebElement dashboard;
     //@FindBy(id = "your-repos-filter") private WebElement searchFilter;
     //@FindBy(xpath = "//li[not(@style)]/a/span/span[@class='repo']") private WebElement repositoryLink;
