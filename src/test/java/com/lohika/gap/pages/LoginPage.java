298635a44d1f52949123662a78123f8d5b93a7d0
vpish <vpish02@hotmail.com>
2017-02-23 13:59:17 +0200

    fixed sig in button

--- src/test/java/com/lohika/gap/pages/LoginPage.java
+++ src/test/java/com/lohika/gap/pages/LoginPage.java
@@ -10,7 +10,7 @@ public class LoginPage extends BasePage {
     @FindBy(id = "login_field") private WebElement loginInput;
     @FindBy(id = "password") private WebElement passInput;
     @FindBy(name = "commit") private WebElement signInButton;
-    @FindBy(css = "a.btn.site-header-actions-btn.mr-1") private WebElement loginButton;
+    @FindBy(linkText = "Sign in") private WebElement loginButton;
     @FindBy (css = "div.flash.flash-full.flash-error") private WebElement loginErrorMessage;
 
     public LoginPage(WebDriver driver) {
